acd8a76dafdd856b85af63a89f493d0967e58106
Rosiele David <rosieleserpa@hotmail.com>
2022-09-16 19:51:26 -0300

    add api para lista de games

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,14 +1,14 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ImageBackground, ImageSourcePropType, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import { ImageBackground, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
-import { THEME } from '../theme';
+import { THEME } from '../../theme';
 import { styles } from './styles';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: { ads: number };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -18,11 +18,11 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
         <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.name}>{data.title}</Text>
 
-          <Text style={styles.ads}>{data.ads} anúncios</Text>
+          <Text style={styles.ads}>{data._count.ads} anúncio(s)</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react';
 import { FlatList, Image, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { Heading } from '../../components/Heading';
-import { GameCard } from '../../GameCard';
 
-import { GAMES } from '../../utils/games';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const response = await fetch('http://192.168.1.9:3333/games');
+      const games = await response.json();
+      setGames(games);
+    };
+
+    fetchGames();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -15,7 +28,7 @@ export function Home() {
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
 
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         showsHorizontalScrollIndicator={false}
