0572bde4e249c27733a6541e85ec919afd15c12a
Rosiele David <rosieleserpa@hotmail.com>
2022-09-16 22:11:28 -0300

    add rotas no app

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { StatusBar } from 'react-native';
 
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black });
@@ -12,7 +12,7 @@ export default function App() {
     <Background>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
