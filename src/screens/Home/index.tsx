d043acb739b3fd3d8171b08f71ccfdd449058527
Rosiele David <rosieleserpa@hotmail.com>
2022-09-16 22:11:01 -0300

    add rota de jogo na home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,11 @@
+import { useNavigation } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { FlatList, Image, View } from 'react-native';
+import { FlatList, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 
+import { Background } from '../../components/Background';
 import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
 
@@ -10,6 +13,11 @@ import { styles } from './styles';
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
+
+  const navigation = useNavigation();
+
+  const handleOpenGame = ({ id, title, bannerUrl }: GameCardProps) =>
+    navigation.navigate('game', { id, title, bannerUrl });
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -22,19 +30,21 @@ export function Home() {
   }, []);
 
   return (
-    <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo} />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logoImg} style={styles.logo} />
 
-      <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
+        <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..." />
 
-      <FlatList
-        data={games}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
+        <FlatList
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <GameCard data={item} onPress={() => handleOpenGame(item)} />}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
